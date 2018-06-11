85cdb339dfa306ff441e7269090efba2c990398a
Patrick Neggie <32044099+patrick9958@users.noreply.github.com>
2018-06-11 11:09:01 -0400

    Revert "replaced RFID folder with TT_RFID"
    This reverts commit cc206bcfce5bc1148619e302a352d0013d1c9972.

---
+++ RFID/examples/AccessControl/AccessControl.ino
@@ -0,0 +1,91 @@
+#include <RFID.h>
+#include <EEPROM.h>     // We are going to read and write PICC's UIDs from/to EEPROM
+#include <SPI.h>        // RC522 Module uses SPI protocol
+#include <MFRC522.h>  // Library for Mifare RC522 Devices
+#include <Arduino.h>
+
+constexpr uint8_t wipeB = 3;     // Button pin for WipeMode
+bool programMode = false;  // initialize programming mode to false
+bool successRead;    // Variable integer to keep if we have Successful Read from Reader
+byte cardID[4];   // Stores scanned ID read from RFID Module
+RFID rfid;
+
+void setup() {
+  //Arduino Pin Configuration
+  pinMode(wipeB, INPUT_PULLUP);   // Enable pin's pull up resistor
+  rfid.initialize();
+  //Protocol Configuration
+  Serial.begin(9600);  // Initialize serial communications with PC
+  //If you set Antenna Gain to Max it will increase reading distance
+  //mfrc522.PCD_SetAntennaGain(mfrc522.RxGain_max);
+  Serial.println(F("Access Control Example v0.1"));
+  rfid.showReaderDetails();  // Show details of Card Reader
+  rfid.toggleDeleteAllRecords(wipeB);
+  bool masterDefined = rfid.isMasterDefined();
+  if(!masterDefined) {
+    rfid.defineMasterCard();
+  }
+  rfid.printInitMessage();
+}
+
+void loop() {
+  successRead = rfid.foundID();
+  rfid.getCardID(cardID);
+  rfid.toggleDeleteMasterCard(wipeB);
+  if(!successRead)
+  {
+    return;
+  }
+  // mode for adding and deleting RFID tags
+  if (programMode)
+  {
+    // When in program mode check First If master card scanned again to exit program mode
+    if ( rfid.isMaster(cardID) )
+    {
+      Serial.println(F("Master Card Scanned"));
+      Serial.println(F("Exiting Program Mode"));
+      Serial.println(F("-----------------------------"));
+      programMode = false;
+      return;
+    }
+    else
+    {
+      if ( rfid.findID(cardID) )
+      { // If scanned card is known delete it
+        Serial.println(F("I know this ID, removing..."));
+        rfid.deleteID(cardID);
+        Serial.println(F("Succesfully removed ID"));
+        Serial.println("-----------------------------");
+        Serial.println(F("Scan an RFID tag to ADD or REMOVE"));
+      }
+      else
+      {                    // If scanned card is not known add it
+        Serial.println(F("I do not know this ID, adding..."));
+        rfid.writeID(cardID);
+        Serial.println(F("Succesfully added ID"));
+        Serial.println(F("-----------------------------"));
+        Serial.println(F("Scan an RFID tag to ADD or REMOVE"));
+      }
+    }
+  }
+
+  // mode for checking IDs to see if they match existing records
+  else {
+    if ( rfid.isMaster(cardID)) {    // If scanned card's ID matches Master Card's ID - enter program mode
+      programMode = true;
+      Serial.println(F("Hello Master - Entered Program Mode"));
+      Serial.println();
+      Serial.println(F("Scan an RFID tag to ADD or REMOVE"));
+      Serial.println(F("Scan Master Card again to Exit Program Mode"));
+      Serial.println(F("-----------------------------"));
+    }
+    else {
+      if ( rfid.findID(cardID) ) { // If not, see if the card is in the records
+        Serial.println(F("Welcome, You shall pass"));
+      }
+      else {      // If not, show that the ID was not valid
+        Serial.println(F("You shall not pass"));
+      }
+    }
+  }
+}
---
+++ RFID/src/RFID.h
@@ -0,0 +1,60 @@
+#ifndef RFID_h
+#define RFID_h
+
+#include "require_cpp11.h"
+#include "deprecated.h"
+// Enable integer limits
+#define __STDC_LIMIT_MACROS
+#include <stdint.h>
+#include <Arduino.h>
+#include <SPI.h>
+#include <EEPROM.h>
+#include <MFRC522.h>
+
+constexpr uint8_t RST_PIN = 9;
+constexpr uint8_t SS_PIN = 10;
+#define MASTER_CARD_SET 143
+
+class RFID {
+public:
+	RFID();  // default RST and SS pins are 9 and 10
+  RFID(uint8_t _RST_PIN, uint8_t _SS_PIN);
+
+	// Not in original AccessControl.ino:
+	/* ------------------------------------------------ */
+	void initialize();				// initialize SPI and RFID module hardware
+	void printInitMessage();	// print to Serial that RFID is initialized
+	void defineMasterCard();	// defines master ID through RFID scanning
+	bool isMasterDefined();		// returns whether or not master ID has been defined
+	// if wipeB is pressed for 10 seconds, all records are wiped
+	void toggleDeleteAllRecords(uint8_t wipeB);
+  void getCardID(byte *_cardID);		// sets _cardID to be the last read ID
+	// if wipeB is pressed for 10 seconds, master ID is wiped
+	void toggleDeleteMasterCard(uint8_t wipeB);
+	void deleteID(byte cardID[4]);	// removes ID from list of passable IDs
+	void writeID(byte cardID[4]);		// adds ID to list of passable IDs
+  void maxRangeOn();		// turns on max range detection for RFID module
+	/* ------------------------------------------------ */
+
+	// In original AccessControl.ino
+	/* ------------------------------------------------ */
+	bool foundID();			// returns true if any ID is read and false otherwise
+	void showReaderDetails();	// prints RFID module data to Serial
+	bool findID(byte find[]);	// finds specific ID
+	bool isMaster(byte test[]);		// returns whether ID is master key or not
+	/* ------------------------------------------------ */
+
+private:
+	void readID(uint8_t number);
+	bool monitorWipeButton(uint32_t interval, uint8_t wipeB);
+  void writeIDTag(byte a[]);
+	void deleteIDTag(byte a[]);
+	bool checkTwo(byte a[], byte b[]);
+	uint8_t findIDSLOT(byte find[]);
+	byte storedCard[4];   // Stores an ID read from EEPROM
+	byte cardID[4];   // Stores scanned ID read from RFID Module
+	byte masterCard[4];   // Stores master card's ID read from EEPROM
+  MFRC522 mfrc522;   // RFID reader/writer module
+};
+
+#endif
